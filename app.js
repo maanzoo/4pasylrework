48c582e76168c48ffa08798b46fd11445df5f370
maanzoo <amanmuhammetallayew@gmail.com>
2023-03-20 02:08:03 +0300

    First update

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 3000, function (req, res) {
+  console.log("Server started running on port 3000");
+});
